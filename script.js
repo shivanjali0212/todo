8e70bd217b4083c8818a12842ac475a3a2839bbc
shivanjali0212 <134398283+shivanjali0212@users.noreply.github.com>
2023-06-06 23:39:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const todosUL = document.getElementById('todos')
+
+const todos = JSON.parse(localstorage.getItem('todos'))
+
+if(todos){
+    todos.forEach(todo => addTodo(todo))}
+
+form.addEventListener('submit',(e)=>{e.preventDefault() 
+    addTodo()})
+
+function addTodo(todo){
+    let todoText=input.value
+    if (todoText){
+        const todoE1=document.createElement('li')}
+    if (todo && todo.completed){    
+        todoE1.classlist.add('completed') }
+    }
+
+todoE1.innerText=todoText  
+todoE1.addEventListener('click' ,()=>{todoE1.classlist.toggle('completed')
+updateLS()
+    })
+
+todoE1.addEventListener('contextmenu',(e)=>{e.preventDefault()
+todoE1.remove()
+updateLS()
+})
+
+function updateLS() {
+    todoE1=document.querySelectorAll('li')
+    const todo =[]
+    todosE1.forEach(todoE1 =>{todos.push({text : todoE1.innerText , completed: todoE1.classlist.contains('completed')})})
+
+localStorage.setItem('todos' , JSON.stringify(todos))
+}
