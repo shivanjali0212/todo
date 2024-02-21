365d44d214f3026eb747f5c823f92be25a5f6810
shivanjali0212 <134398283+shivanjali0212@users.noreply.github.com>
2024-02-21 21:06:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,16 @@ const todos = JSON.parse(localstorage.getItem('todos'))
 
 if(todos){
     todos.forEach(todo => addTodo(todo))}
-
-form.addEventListener('submit',(e)=>{e.preventDefault()
-    addTodo()})
+    form.addEventListener('submit',(e)=>{e.preventDefault()
+    addTodo()
+  })
 
 function addTodo(todo){
     let todoText=input.value
     if (todoText){
         const todoE1=document.createElement('li')}
     if (todo && todo.completed){    
-        todoE1.classlist.add('completed') }
-    }
+        todoE1.classlist.add('completed') }}
 
 todoE1.innerText=todoText  
 todoE1.addEventListener('click' ,()=>{todoE1.classlist.toggle('completed')
